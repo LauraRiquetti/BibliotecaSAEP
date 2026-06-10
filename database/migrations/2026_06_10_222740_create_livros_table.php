7c0c75202837d82ff5b0b2c708bb057309ff1ee4
LauraRiquetti <laurariquetti@gmail.com>
2026-06-10 19:49:54 -0300

    Initial commit

---
+++ database/migrations/2026_06_10_222740_create_livros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('autor');
+            $table->integer('ano');
+            $table->string('editora');
+            $table->string('categoria');
+            $table->integer('quantidade');
+            $table->string('status')->default('disponível');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
